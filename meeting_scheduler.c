ff89822c74397043cdc56bacbb6f05057662ad62
sachinhansda-ally <sachin.hansda@ally.io>
2021-08-27 20:41:26 +0530

    time, employee, meeting structures

--- meeting_scheduler.c
+++ meeting_scheduler.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
+#include<time.h>
 
 const int M = 1;
 const int N = 5;
+
+struct time
+{
+  int hour, minutes;
+};
+
+struct employee
+{
+  int employee_id;
+  char employee_name[30];
+};
+
+struct meeting
+{
+  int meeting_id, organiser_id, room_no;
+  struct time start_time, end_time;
+};
 
 int main()
 {
