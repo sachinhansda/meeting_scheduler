f73cef10df77f47b785b5eb86e32fe011681019f
sachinhansda-ally <sachin.hansda@ally.io>
2021-08-28 19:12:48 +0530

    cancel meeting function

--- meeting_scheduler.c
+++ meeting_scheduler.c
@@ -4,6 +4,8 @@
 const int M = 1;
 const int N = 5;
 
+int meeting_size;
+
 struct time
 {
   int hour, minutes;
@@ -28,14 +30,13 @@ void book(int organiser_id, struct time start_time, struct time end_time)
   {
     if(free_rooms[i]==0)
     {
-      size = sizeof(meetings)/sizeof(struct meeting);
-      meetings[size].meeting_id = size;
-      meetings[size].organiser_id = organiser_id;
-      meetings[size].room_no = i;
-      meetings[size].start_time.hour = start_time.hour;
-      meetings[size].start_time.minutes = start_time.minutes;
-      meetings[size].end_time.hour = end_time.hour;
-      meetings[size].end_time.minutes = end_time.minutes;
+      meetings[meeting_size].meeting_id = size;
+      meetings[meeting_size].organiser_id = organiser_id;
+      meetings[meeting_size].room_no = i;
+      meetings[meeting_size].start_time.hour = start_time.hour;
+      meetings[meeting_size].start_time.minutes = start_time.minutes;
+      meetings[meeting_size].end_time.hour = end_time.hour;
+      meetings[meeting_size].end_time.minutes = end_time.minutes;
       printf("Employee %d: (%d:%d - %d:%d at Room %d) success\n",  meetings[size].organiser_id, meetings[size].start_time.hour, meetings[size].start_time.minutes, meetings[size].end_time.hour, meetings[size].end_time.minutes, meetings[size].room_no);
       break;
     }
@@ -44,7 +45,30 @@ void book(int organiser_id, struct time start_time, struct time end_time)
 
 void cancel(int employee_id, int meeting_id)
 {
-
+  int i, size;
+  size = sizeof(meetings)/sizeof(struct meeting);
+  for(i=0; i<meeting_size; i++)
+  {
+    if(meetings[i].meeting_id == meeting_id)
+    {
+      if(meetings[i].organiser_id != employee_id)
+      {
+        printf("You are not the organizer of this meeting, hence you cannot cancel the meeting\n");
+      }
+      else
+      {
+        meetings[i].meeting_id = meetings[meeting_size].meeting_id;
+        meetings[i].organiser_id = meetings[meeting_size].organiser_id;
+        meetings[i].room_no = meetings[meeting_size].room_no;
+        meetings[i].start_time.hour = meetings[meeting_size].start_time.hour;
+        meetings[i].start_time.minutes = meetings[meeting_size].start_time.minutes;
+        meetings[i].end_time.hour = meetings[meeting_size].end_time.hour;
+        meetings[i].end_time.minutes = meetings[meeting_size].end_time.minutes;
+        meeting_size--;
+        printf("Success, meeting canceled");
+      }
+    }
+  }
 }
 
 int main()
